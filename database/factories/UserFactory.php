997409de68204528dcaf7fe9a06451f432d05f6c
SAREMY-9 <brianowino226@gmail.com>
2025-11-07 09:50:01 +0300

    UserFactory with faker and use at the top-2

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,8 +5,8 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
-use function fake;
 
+use function fake;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -29,7 +29,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => static::$password ??= Hash::make('password'), // default password: "password"
             'remember_token' => Str::random(10),
         ];
     }
