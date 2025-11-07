1f16932d6a39fa739321bc5eb590ec78d89be67e
SAREMY-9 <brianowino226@gmail.com>
2025-11-07 10:42:10 +0300

    UserFactory with faker and use at the top-6

--- database/seeders/LocationSeeder.php
+++ database/seeders/LocationSeeder.php
@@ -1,18 +1,45 @@
 <?php
 
-namespace Database\Seeders;
+namespace Database\Factories;
 
-use Illuminate\Database\Seeder;
-use App\Models\Location;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use App\Models\User;
 
-class LocationSeeder extends Seeder
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends Factory
 {
-    public function run()
+    /**
+     * The name of the factory's corresponding model.
+     */
+    protected $model = User::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
     {
-        Location::insert([
-            ['name' => 'Nairobi', 'created_at' => now(), 'updated_at' => now()],
-            ['name' => 'Mombasa', 'created_at' => now(), 'updated_at' => now()],
-            ['name' => 'Kisumu', 'created_at' => now(), 'updated_at' => now()],
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'), // default password
+            'remember_token' => Str::random(10),
+        ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     */
+    public function unverified(): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'email_verified_at' => null,
         ]);
     }
 }
